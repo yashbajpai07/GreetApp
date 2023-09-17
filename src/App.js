686232e1d2b9cd082f8616ba06cef3bd9b330824
Yash Bajpai <yashbajpaiys@gmail.com>
2023-09-17 22:52:32 +0530

    Greet App Practice React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+
+import {useState} from "react";
+import "./App.css";
 
 function App() {
+
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [message, setMessage] = useState(false);
+  
+
+  const handleClickokay=()=>{
+    const fn=firstName;
+    const ln=lastName;
+    setMessage(!message);
+    console.log(fn + ln);
+  }
+
+  const handleClickclear=()=>{
+    setFirstName("");
+    setLastName("");
+    setMessage(false);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Greet App</h1>
+      <div className="input-group">
+        <span className="input-group-text">First and last name</span>
+        <input onChange={(e)=>setFirstName(e.target.value)} type="text"  className="form-control" />
+        <input onChange={(e)=>setLastName(e.target.value)} type="text"  className="form-control" />
+      </div>
+      <button type="button" class="btn btn-primary" onClick={handleClickokay}>
+       Okay
+      </button>
+      <button type="button" class="btn btn-secondary" onClick={handleClickclear}>
+        Clear
+      </button>
+      {message && <p>{`hello ${firstName} ${lastName}`}</p>}
     </div>
   );
 }
